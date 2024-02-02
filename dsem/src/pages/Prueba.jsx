615843928599704437ade37f7070ed7bf2151eda
Andres Daza <cdazaparra@gmail.com>
2024-02-02 07:27:48 -0500

    pull

--- dsem/src/pages/Prueba.jsx
+++ dsem/src/pages/Prueba.jsx
@@ -7,27 +7,31 @@ import Gatico2 from "../assets/images/gatico2.jpg"
 const Prueba = () => {
   return (
     <div className='Prueba'>
-    <hr></hr>
-    <hr></hr>
-    <h2>prueba</h2>
-    <div className='pruebaCard'>
-    <h3>Esto es un ejemplo de un subtítulo</h3>
-      <div  className='pruebacardData'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum incidunt sint eius. Optio aperiam blanditiis maiores ex reprehenderit iste, omnis ratione delectus mollitia consectetur. Doloribus veritatis consequuntur et voluptas qui!</p>
-      <img className='pruebacarddataImg' src={Gatico} alt='Esto es un arbolito'></img>
+      <hr></hr>
+      <hr></hr>
+      <h2>prueba</h2>
+      <div className='pruebaCard'>
+        <h3>
+          Esto es un ejemplo de un subtítulo
+        </h3>
+        <div className='pruebacardData'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum incidunt sint eius. Optio aperiam blanditiis maiores ex reprehenderit iste, omnis ratione delectus mollitia consectetur. Doloribus veritatis consequuntur et voluptas qui!
+          </p>
+          <img className='pruebacarddataImg' src={Gatico} alt='Esto es un arbolito'></img>
+        </div>
       </div>
-    </div>
-    <hr></hr>
-    <h2>Ejemplo 2</h2>
-    <div className='pruebaCard'>
-    <h3>Esto es un ejemplo de un subtítulo</h3>
-      <div  className='pruebacardData'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum incidunt sint eius. Optio aperiam blanditiis maiores ex reprehenderit iste, omnis ratione delectus mollitia consectetur. Doloribus veritatis consequuntur et voluptas qui!</p>
-      <img className='pruebacarddataImg' src={Gatico2} alt='Esto es un arbolito'></img>
+      <hr></hr>
+      <h2>Ejemplo 2</h2>
+      <div className='pruebaCard'>
+        <h3>Esto es un ejemplo de un subtítulo</h3>
+        <div className='pruebacardData'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum incidunt sint eius. Optio aperiam blanditiis maiores ex reprehenderit iste, omnis ratione delectus mollitia consectetur. Doloribus veritatis consequuntur et voluptas qui!</p>
+          <img className='pruebacarddataImg' src={Gatico2} alt='Esto es un arbolito'></img>
+        </div>
       </div>
+      <hr></hr>
     </div>
-    <hr></hr>
-  </div>
   )
 }
 
